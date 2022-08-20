127b62fa777883e26b41647b19cb30d25df10821
Kai Yang Lim <lim_kaiyang@hotmail.com>
2022-08-20 19:22:46 +1000

    search page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import HomePage from './pages/HomePage';
-import NewPage from './pages/NewPage';
-import ProfilePage from './pages/ProfilePage';
-import ScenicPage from './pages/ScenicPage';
-import MobileHackathon from './components/react-mobile-hackathon';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import HomePage from "./pages/HomePage";
+import NewPage from "./pages/NewPage";
+import ProfilePage from "./pages/ProfilePage";
+import ScenicPage from "./pages/ScenicPage";
+import MobileHackathon from "./components/react-mobile-hackathon";
+import { Route, Switch } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
 
 class App extends Component {
-    render() {
-        return (
-            <MobileHackathon appName="Cycl" displayUrl="cycl.com">
-              <Switch>
-                <Route path='/new' component={NewPage} />
-                <Route path='/profile' component={ProfilePage} />
-                <Route path='/scenic' component={ScenicPage} />
-                <Route exact path='/' component={HomePage} />
-              </Switch>
-            </MobileHackathon>
-        );
-    }
+  render() {
+    return (
+      <MobileHackathon appName="Cycl" displayUrl="cycl.com">
+        <Switch>
+          <Route path="/new" component={NewPage} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/scenic" component={ScenicPage} />
+          <Route path="/search" component={SearchPage} />
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </MobileHackathon>
+    );
+  }
 }
 
 export default App;
