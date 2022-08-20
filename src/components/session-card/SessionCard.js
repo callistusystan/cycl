c6d993ec5539a3fa36f058dec7ca4c34cafa0777
Kai Yang Lim <lim_kaiyang@hotmail.com>
2022-08-20 15:19:17 +1000

    Session Card

---
+++ src/components/session-card/SessionCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import circles from "../assets/circles.svg";
+
+const SessionCard = ({ title, description, numMembers, onClick }) => {
+  return (
+    <Card onClick={onClick}>
+      <p style={{ font: "var(--mini-title)", margin: "0 0 4px" }}>{title}</p>
+      <p style={{ font: "var(--copy14)", margin: "0 0 8px" }}>{description}</p>
+      <div style={{ display: "flex", gap: "4px" }}>
+        <img src={circles} alt="" />
+        <p
+          style={{ font: "var(--copy12)", color: "var(--slate)", margin: "0" }}
+        >
+          {numMembers} members
+        </p>
+      </div>
+    </Card>
+  );
+};
+
+export default SessionCard;
