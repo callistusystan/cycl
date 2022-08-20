3c0b6c2a8cb64ea7d262d57b58376ea222785383
Kai Yang Lim <lim_kaiyang@hotmail.com>
2022-08-20 20:07:00 +1000

    Completed ride page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import HomePage from './pages/HomePage';
-import NewPage from './pages/NewPage';
-import ProfilePage from './pages/ProfilePage';
-import ScenicPage from './pages/ScenicPage';
-import ScenicRidingPage from './pages/ScenicRidingPage';
+import React, { Component } from "react";
+import HomePage from "./pages/HomePage";
+import NewPage from "./pages/NewPage";
+import ProfilePage from "./pages/ProfilePage";
+import ScenicPage from "./pages/ScenicPage";
+import ScenicRidingPage from "./pages/ScenicRidingPage";
 import SearchPage from "./pages/SearchPage";
-import MobileHackathon from './components/react-mobile-hackathon';
-import { Route, Switch } from 'react-router-dom';
+import MobileHackathon from "./components/react-mobile-hackathon";
+import { Route, Switch } from "react-router-dom";
+import CompletedRidePage from "./pages/CompletedRide";
 
 class App extends Component {
   render() {
@@ -16,8 +17,9 @@ class App extends Component {
           <Route path="/new" component={NewPage} />
           <Route path="/profile" component={ProfilePage} />
           <Route path="/scenic" component={ScenicPage} />
-          <Route path='/scenic_riding' component={ScenicRidingPage} />
+          <Route path="/scenic_riding" component={ScenicRidingPage} />
           <Route path="/search" component={SearchPage} />
+          <Route path="/completed-ride" component={CompletedRidePage} />
           <Route exact path="/" component={HomePage} />
         </Switch>
       </MobileHackathon>
